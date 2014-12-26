bb5fcbabc7173a545e7b043cd49979915e27256f
Aercio Melo <aercio.melo@cesar.org.br>
2014-12-26 14:41:41 -0300

    Location Services adicionado

--- app/src/main/java/br/com/fabricam8/seniorsapp/BaseActivity.java
+++ app/src/main/java/br/com/fabricam8/seniorsapp/BaseActivity.java
@@ -3,9 +3,12 @@ package br.com.fabricam8.seniorsapp;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +16,23 @@ public abstract class BaseActivity extends Activity {
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
+    /**
+     * Sets the header title. When available.
+     * @param title The title to be set.
+     */
+    protected void setHeaderTitle(String title) {
+        TextView vTitle = (TextView) findViewById(R.id.header_label_title);
+        if(vTitle != null) {
+            vTitle.setText(title);
+        }
+    }
+
+    /**
+     * Returns to the previous activity.
+     *
+     * @param v The view requested the method.
+     */
+    public void previous(View v) {
+        finish();
+    }
 }
--- app/src/main/java/br/com/fabricam8/seniorsapp/DashboardActivity.java
+++ app/src/main/java/br/com/fabricam8/seniorsapp/DashboardActivity.java
@@ -1,6 +1,9 @@
 package br.com.fabricam8.seniorsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends BaseActivity {
 
@@ -8,5 +11,15 @@ public class DashboardActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        Button btGps = (Button) findViewById(R.id.dashButtonGps);
+        btGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
