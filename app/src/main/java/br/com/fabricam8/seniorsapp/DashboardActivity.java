dd9cdea862ca0f5894ba367c2fd90a0781ac247a
Aercio Melo <aercio.melo@cesar.org.br>
2014-12-26 15:49:46 -0300

    Comentarios adicionados

--- app/src/main/java/br/com/fabricam8/seniorsapp/DashboardActivity.java
+++ app/src/main/java/br/com/fabricam8/seniorsapp/DashboardActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class DashboardActivity extends BaseActivity {
 
@@ -12,23 +11,22 @@ public class DashboardActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-
-//        Button btGps = (Button) findViewById(R.id.dashButtonGps);
-//        btGps.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
     }
 
+    /**
+     * Invoked when clicked on the dashboard.
+     * @param v The button which invoked the action.
+     */
     public void openLocation(View v)
     {
-        Intent intent = new Intent(DashboardActivity.this, LocationActivity.class);
-        startActivity(intent);
+        Intent i = new Intent(DashboardActivity.this, LocationActivity.class);
+        startActivity(i);
     }
 
+    /**
+     * Invoked when clicked on the dashboard.
+     * @param v The button which invoked the action.
+     */
     public void openBrowser(View v)
     {
         Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com/"));
