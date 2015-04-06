9f7c0c5de742027e4b62970a48006df5de3dc299
Carlos Roberto <carlosdombosco@gmail.com>
2015-04-06 19:56:54 -0300

    Correção do Bug: Chamada do título da atividade na tela de alteração

--- app/src/main/java/br/com/fabricam8/seniorsapp/ExerciseInfoActivity.java
+++ app/src/main/java/br/com/fabricam8/seniorsapp/ExerciseInfoActivity.java
@@ -126,7 +126,7 @@ public class ExerciseInfoActivity extends ActionBarActivity {
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
             FormHelper.setTextBoxValue(this, R.id.exe_info_start_date, "Iniciar em: " + dateFormat.format(sessionExercise.getStartDate()));
-            FormHelper.setTextBoxValue(this, R.id.exe_info_end_date, "Finalizar em: " + dateFormat.format(sessionExercise.getEndDate()));
+          //  FormHelper.setTextBoxValue(this, R.id.exe_info_end_date, "Finalizar em: " + dateFormat.format(sessionExercise.getEndDate()));
 
             // horarios
             SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
