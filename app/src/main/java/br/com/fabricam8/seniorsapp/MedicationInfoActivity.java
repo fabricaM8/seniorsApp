e059f24d151bcee2c3733d37857473cdd508f953
Carlos Roberto <carlosdombosco@gmail.com>
2015-04-06 23:12:27 -0300

    Correção do Bug: Alteração das perguntas na confirmação de deleção

--- app/src/main/java/br/com/fabricam8/seniorsapp/MedicationInfoActivity.java
+++ app/src/main/java/br/com/fabricam8/seniorsapp/MedicationInfoActivity.java
@@ -100,7 +100,7 @@ public class MedicationInfoActivity extends ActionBarActivity {
 
         new AlertDialog.Builder(this)
                 .setTitle("Confirme")
-                .setMessage("Deseja realmente deletar medicação?")
+                .setMessage("Deseja realmente excluir a medicação?")
                 .setIcon(R.drawable.ic_action_alert)
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
