e5c73e13b2df18af29d6d63f533ec7eb61243d57
Laécyo Marcello <laecyomarcello@gmail.com>
2015-05-18 20:50:24 -0300

    .

--- app/src/main/java/br/com/fabricam8/seniorsapp/EventsListSaude.java
+++ app/src/main/java/br/com/fabricam8/seniorsapp/EventsListSaude.java
@@ -29,6 +29,7 @@ public class EventsListSaude extends ActionBarActivity {
 
         super.onCreate(savedInstanceState);
 
+        
         setContentView(R.layout.activity_saude_list);
 
         // create toolbar
